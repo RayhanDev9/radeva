1f0dfbb9117749264a5c1043555ac0e643420eb1
Muhamad Rayhan <m.rayhanoi26@gmail.com>
2026-06-01 07:26:52 +0700

    refector : clean code

--- js/dataFAQ.js
+++ js/dataFAQ.js
@@ -1,24 +1,24 @@
 export const dataFAQ = {
   general: [
     {
-      question: 'Apakah Produk Kami Halal?',
+      question: 'Apakah Produk Kami Halal ?',
       answer: 'Semua produk yang kami jual telah terjamin kehalalannya dan memiliki logo Halal resmi pada kemasan masing-masing produk.'
     },
     {
-      question: 'Bagaimana Cara Melakukan Pesanan?',
+      question: 'Bagaimana Cara Melakukan Pesanan ?',
       answer: 'Kami menyediakan dua cara pemesanan resmi: belanja langsung via Shopee untuk menikmati promo gratis ongkir, atau hubungi Chat Admin untuk pemesanan manual dan bantuan'
     } ,
     {
-      question : 'Bagaimana Cara Menjadi Reseller?',
+      question : 'Bagaimana Cara Menjadi Reseller ?',
       answer : 'Buka peluang kemitraan menjadi reseller resmi kami! Caranya praktis, cukup kirimkan foto KTP dan NPWP Anda ke tim Admin untuk proses verifikasi data.'
     },
     {
-      question: 'Bagaimana Cara Konsultasi?',
+      question: 'Bagaimana Cara Konsultasi ?',
       answer : 'Nikmati konsultasi produk gratis melalui WhatsApp di nomor +62 895-3843-9000. Layanan kami tersedia setiap hari pukul 08:00 hingga 19:00 WIB.'
     }
   ],
   order: [{
-    question : 'Metode Pembayaran Yang Tersedia ?',
+    question : 'Metode Pembayaran Yang Tersedia',
     answer : 'Demi keamanan, kami hanya menerima transfer via Bank BCA resmi yang tertera saat pembelian. Kami tidak bertanggung jawab dan tidak dapat memproses transaksi yang dikirimkan ke rekening di luar akun resmi kami.'
   }
   ],
@@ -28,7 +28,7 @@ export const dataFAQ = {
       answer : 'Estimasi pengiriman 1–2 hari untuk dalam kota dan 1–5 hari untuk luar kota tergantung ekspedisi.'
     },
     {
-      question : 'Apakah Geratis Ongkir',
+      question : 'Apakah Geratis Ongkir ?',
       answer : 'Untuk transaksi di luar platform Shopee, biaya pengiriman (ongkir) akan dibebankan kepada pihak pembeli sesuai dengan tarif ekspedisi yang digunakan.'
     }
   ],
